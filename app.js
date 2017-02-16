075fb03042bb6d625b92faf1e81c6a3419b84a70
ghigbie <gehigbie@gmail.com>
2017-02-16 03:30:38 +0000

    added root route and pageNotFound route

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var app = express();
 
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("*", function(req, res){
+    res.render("pageNotFound");
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The server is listening...Yay! Let's celebrate!");
 });
