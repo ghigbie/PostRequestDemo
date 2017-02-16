fc5c9e602052a78f4b7654a581f4aca0523dc75f
ghigbie <gehigbie@gmail.com>
2017-02-16 03:27:36 +0000

    added view engine and set server to listen

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "ejs");
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("The server is listening...Yay! Let's celebrate!");
+});
